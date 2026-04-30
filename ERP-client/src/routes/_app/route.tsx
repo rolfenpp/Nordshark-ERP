5c34bee4cfe201f88370e8e7d6ba9d8e3276bb6a
rolfenpp <isak@isakberg.dev>
2026-04-30 18:09:46 +0200

    skeleton improvement

--- ERP-client/src/routes/_app/route.tsx
+++ ERP-client/src/routes/_app/route.tsx
@@ -1,23 +1,18 @@
-import { createFileRoute, Outlet, useRouterState } from '@tanstack/react-router'
+import { createFileRoute, Outlet } from '@tanstack/react-router'
 import { AppToastContainer } from '@/components/AppToastContainer'
 import { ProtectedRoute } from '@/components/ProtectedRoute'
 import { DashboardLayout } from '@/components/DashboardLayout'
-import { RoutePageSkeleton } from '@/components/Skeletons'
 
 export const Route = createFileRoute('/_app')({
   component: AppLayoutComponent,
 })
 
 function AppLayoutComponent() {
-  const routeBusy = useRouterState({
-    select: (s) => s.isLoading || s.status === 'pending',
-  })
-
   return (
     <ProtectedRoute>
       <DashboardLayout>
         <AppToastContainer />
-        {routeBusy ? <RoutePageSkeleton /> : <Outlet />}
+        <Outlet />
       </DashboardLayout>
     </ProtectedRoute>
   )
